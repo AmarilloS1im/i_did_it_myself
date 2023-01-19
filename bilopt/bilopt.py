57a23bfb1ed1b8e2102830a67d03d066b8fbb066
Alex PC <efanovsj@gmail.com>
2023-01-19 15:59:51 +0300

    Refactoring code 19 01 2023

--- bilopt/bilopt.py
+++ bilopt/bilopt.py
@@ -1,5 +1,5 @@
 '''
-В файл Task_from_PVB.xlsx проставить нужные артикулы для поиска в первый столбец. Всю остальную информацию удалить(если она есть)
+В файл Task_from_PVB.xlsx проставить нужные артикулы для поиска в первый столбец. Шапку с заголовками оставить. Всю остальную информацию удалить(если она есть)
 
 Артикулы вставлять как специальная вставка
 
@@ -43,7 +43,6 @@ def ProcessedString(non_processed_list):
     return processed_list
 def GetData(session,processed_list,headers):
     brand = str(input('Введите название бренда,например Denso (Регистр имеет значение, вводить как на сайте BilOpt): '))
-    count_list = []
     total_list = []
     qty_list_total = []
     for articles in processed_list:
@@ -51,44 +50,54 @@ def GetData(session,processed_list,headers):
         url = f"https://www.bilopt.ru/Search/GetFindHeaders?productId=&number={articles}"
         response = session.get(url=url, headers=headers)
         user_friendly_json = json.loads(response.text)
+        if len(user_friendly_json["ProductLists"]) == 0 or user_friendly_json["ProductLists"][0]['Groups'][0]['Manufacturers'] == None :
+            info_list.append('Нет данных на сайте')
+            info_list.append('Нет данных на сайте')
+            info_list.append(0)
+            info_list.append(0)
+            info_list.append(0)
+            info_list.append(0)
+            info_list.append(0)
+            info_list.append(0)
+            total_list.append(info_list)
         for x in range(len(user_friendly_json['ProductLists'])):
-            if user_friendly_json["ProductLists"][x]['Groups'][0]['Manufacturers'] == None:
-                continue
-            current_brand = user_friendly_json["ProductLists"][x]['Groups'][0]['Manufacturers'][0]
-
-            if current_brand != brand:
-                pass
-            else:
-                count_list.append(user_friendly_json["ProductLists"][x]['Groups'][0]['Manufacturers'][0])
-                product_id = user_friendly_json["ProductLists"][x]['Groups'][0]['Products'][0]['ProductId']
-                info_list.append(user_friendly_json["ProductLists"][x]['Groups'][0]['Manufacturers'][0])
-                info_list.append(user_friendly_json["ProductLists"][x]['Groups'][0]['Products'][0]["ProductNumber"])
-                info_list.append(math.ceil(user_friendly_json["ProductLists"][x]['Groups'][0]['Products'][0]["MinimalPrice"]))
-                info_list.append(user_friendly_json["ProductLists"][x]['Groups'][0]['Products'][0]["MaximumPrice"])
-                url_qty_inf = f'https://www.bilopt.ru/Search/GetFindOffers?productId=&number={articles}&city=&selectedProductId={product_id}'
-                response_qty = session.get(url=url_qty_inf,headers=headers)
-                user_friendly_json_qty = json.loads(response_qty.text)
-                for x in range (len(user_friendly_json_qty['Items'])):
-                    if user_friendly_json_qty['Items'][x]['Quantity'] == '' or  user_friendly_json_qty['Items'][x]['Quantity'] == None:
+            if user_friendly_json["ProductLists"][x]['Groups'][0]['Manufacturers'] != None:
+                for j in range(len(user_friendly_json["ProductLists"][x]['Groups'][0]['Manufacturers'])):
+                    if user_friendly_json["ProductLists"][x]['Groups'][0]['Manufacturers'] == None:
+                        continue
+                    current_brand = user_friendly_json["ProductLists"][x]['Groups'][0]['Manufacturers'][j]
+                    if current_brand != brand:
                         pass
                     else:
-                        qty_list_total.append(int(user_friendly_json_qty['Items'][x]['Quantity']))
-                if len(qty_list_total) == 0:
-                    max_qty = 0
-                    min_qty = 0
-                    average_qty = 0
-                    total_qty = 0
-                else:
-                    max_qty = max(qty_list_total)
-                    min_qty = min(qty_list_total)
-                    average_qty = round(sum(qty_list_total)/len(qty_list_total))
-                    total_qty = sum(qty_list_total)
-                info_list.append(max_qty)
-                info_list.append(min_qty)
-                info_list.append(average_qty)
-                info_list.append(total_qty)
-                total_list.append(info_list)
-                qty_list_total=[]
+                        product_id = user_friendly_json["ProductLists"][x]['Groups'][0]['Products'][0]['ProductId']
+                        info_list.append(user_friendly_json["ProductLists"][x]['Groups'][0]['Manufacturers'][j])
+                        info_list.append(user_friendly_json["ProductLists"][x]['Groups'][0]['Products'][0]["ProductNumber"])
+                        info_list.append(math.ceil(user_friendly_json["ProductLists"][x]['Groups'][0]['Products'][0]["MinimalPrice"]))
+                        info_list.append(user_friendly_json["ProductLists"][x]['Groups'][0]['Products'][0]["MaximumPrice"])
+                        url_qty_inf = f'https://www.bilopt.ru/Search/GetFindOffers?productId=&number={articles}&city=&selectedProductId={product_id}'
+                        response_qty = session.get(url=url_qty_inf,headers=headers)
+                        user_friendly_json_qty = json.loads(response_qty.text)
+                        for x in range (len(user_friendly_json_qty['Items'])):
+                            if user_friendly_json_qty['Items'][x]['Quantity'] == '' or  user_friendly_json_qty['Items'][x]['Quantity'] == None:
+                                pass
+                            else:
+                                qty_list_total.append(int(user_friendly_json_qty['Items'][x]['Quantity']))
+                        if len(qty_list_total) == 0:
+                            max_qty = 0
+                            min_qty = 0
+                            average_qty = 0
+                            total_qty = 0
+                        else:
+                            max_qty = max(qty_list_total)
+                            min_qty = min(qty_list_total)
+                            average_qty = round(sum(qty_list_total)/len(qty_list_total))
+                            total_qty = sum(qty_list_total)
+                        info_list.append(max_qty)
+                        info_list.append(min_qty)
+                        info_list.append(average_qty)
+                        info_list.append(total_qty)
+                        total_list.append(info_list)
+                        qty_list_total=[]
     return total_list
 
 
