01e7b419f90bdcd1b071b0caf23b85d30931bf68
Alex PC <efanovsj@gmail.com>
2023-03-12 20:41:39 +0300

    RefactorFTS_ OVAR

--- FTS/FTS_OVAR.py
+++ FTS/FTS_OVAR.py
@@ -6,17 +6,18 @@ from  email.mime.base import MIMEBase
 from email import encoders
 import openpyxl
 from openpyxl import load_workbook
+"""
+Before start script, always remove file 'Final_stat_form Счет-фактура № xxx от xx.xx.xxxx г..xlsx' from folder!
+"""
 class Country:
-    def __repr__(self):
-        return self.code + ' ' + self.alfa_2 + ' ' + self.short_name
-
-    def __init__(self,code,short_name,alfa_2):
+    def __init__(self,short_name_rus,code,alfa_2,short_name_en):
+        self.short_name_rus = short_name_rus
         self.code = code
-        self.short_name = short_name
         self.alfa_2 = alfa_2
-class Row:
-    def __init__(self,row):
-        self.row = row
+        self.short_name_en = short_name_en
+    def __repr__(self):
+        return self.short_name_rus + ' ' + self.code + ' ' + self.alfa_2 + ' ' + self.short_name_en
+
 class Tnvd:
     def __init__(self,discription, tnvd_code):
         self.discription = discription
@@ -26,6 +27,7 @@ def main():
     sendMesageToMail()
 
 def getInfoFromSF():
+    doc_currency = str(input('Валюта документа рубли? y/n or/или да/нет'))
     rub_eur = float(input('Введите курс RUB к ЕВРО '))
     rub_usd = float(input('Введите курс RUB к USD '))
     lable = str(input('Введите бренд/торговую марку товара, например: Denso '))
@@ -42,47 +44,51 @@ def getInfoFromSF():
     file = open("country.csv", 'r')
     for line in file:
         read_line_list = line.split(';')
-        tmp_country = Country(read_line_list[3].replace('"', ''), read_line_list[0].replace('"', ''),
-                              read_line_list[4].replace('"', ''))
-        country_dict[tmp_country.alfa_2] = tmp_country
+        tmp_country = Country(read_line_list[0].replace('"', ''), read_line_list[3].replace('"', ''),
+                              read_line_list[4].replace('"', ''), read_line_list[6].replace('"', ''))
+        country_dict[tmp_country.short_name_en] = tmp_country
 
     book = openpyxl.open('SF.xlsx', read_only=True, data_only=True)
     sheet = book.active
-    row = Row(16)
+    row = 16
     data_list = []
     temp_data_list = []
+    sf_number = sheet[3][0].value
 
     for x in range(16,(sheet.max_row)-10):
         for column in range(1,18):
-            temp_data_list.append(sheet[row.row][column].value)
+            temp_data_list.append(sheet[row][column].value)
         data_list.append(temp_data_list)
         temp_data_list = []
-        row.row +=1
+        row +=1
     data_list = data_list[3:]
     for x in range(len(data_list)):
         data_list[x][1] = str(data_list[x][1])
     book.close()
-    shutil.copy('Stat_form.xlsx', 'Final_stat_form.xlsx')
-    work_book = openpyxl.open('Final_stat_form.xlsx', read_only=False, data_only=True)
+    shutil.copy('Stat_form.xlsx', f'Final_stat_form {sf_number}.xlsx')
+    work_book = openpyxl.open(f'Final_stat_form {sf_number}.xlsx', read_only=False, data_only=True)
     work_book_sheet = work_book.active
-    row.row = 0
+    row = 0
 
     for x in range(19,(sheet.max_row)-10):
         for column in range(1,23):
-            work_book_sheet[row.row + 2][1].value = data_list[row.row][0] + " " + str(data_list[row.row][1])
-            work_book_sheet[row.row + 2][0].value = tnvd_dict[data_list[row.row][1]].tnvd_code
-            work_book_sheet[row.row + 2][10].value = data_list[row.row][15]
-            work_book_sheet[row.row + 2][12].value = country_dict[data_list[row.row][16]].short_name
-            work_book_sheet[row.row + 2][11].value = data_list[row.row][16]
-            work_book_sheet[row.row + 2][13].value = data_list[row.row][3]
-            work_book_sheet[row.row + 2][14].value = data_list[row.row][2]
-            work_book_sheet[row.row + 2][15].value = '796'
-            work_book_sheet[row.row + 2][16].value = round(data_list[row.row][9],2)
-            work_book_sheet[row.row + 2][17].value = round(((data_list[row.row][9]) * rub_eur)/rub_usd,2)
-            work_book_sheet[row.row + 2][18].value = round(data_list[row.row][9] * rub_eur, 2)
-            work_book_sheet[row.row + 2][20].value = data_list[row.row][13]
-        row.row +=1
-    work_book.save('Final_stat_form.xlsx')
+            work_book_sheet[row + 2][1].value = data_list[row][0] + " " + str(data_list[row][1])
+            work_book_sheet[row + 2][0].value = tnvd_dict[data_list[row][1]].tnvd_code
+            work_book_sheet[row + 2][10].value = data_list[row][15]
+            work_book_sheet[row + 2][12].value = country_dict[(data_list[row][16].upper())].short_name_rus
+            work_book_sheet[row + 2][11].value = data_list[row][16]
+            work_book_sheet[row + 2][13].value = data_list[row][3]
+            work_book_sheet[row + 2][14].value = data_list[row][2]
+            work_book_sheet[row + 2][15].value = '796'
+            work_book_sheet[row + 2][16].value = round(data_list[row][9],2)
+            if doc_currency == 'y' or doc_currency == 'да':
+                work_book_sheet[row + 2][17].value = round(data_list[row][9] / rub_usd, 2)
+            else:
+                work_book_sheet[row + 2][17].value = round(((data_list[row][9]) * rub_eur)/rub_usd,2)
+            work_book_sheet[row + 2][18].value = round(data_list[row][9] * rub_eur, 2)
+            work_book_sheet[row + 2][20].value = data_list[row][13]
+        row +=1
+    work_book.save(f'Final_stat_form {sf_number}.xlsx')
     work_book.close()
 
 
